89e2b8310c3dc9482dd1177c6c4be6bf20dd82cf
songjian <songjian@baozou.com>
2016-04-21 16:36:12 +0800

    js

--- config/application.rb
+++ config/application.rb
@@ -21,29 +21,7 @@ module PushWeb
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.precompile << proc.new do |path|
-      if path =~ /\.(css|js |scss|png|jpg|gif|js on)\z/
-        full_path = rails.application.assets.resolve(path).to_path
-        app_assets_path1 = rails.root.join('app', 'assets').to_path
-        app_assets_path2 = rails.root.join('public', 'assets').to_path
-        app_assets_path3 = rails.root.join('vendor', 'assets').to_path
 
-        if full_path.starts_with? app_assets_path1
-           true
-        else
-          if full_path.starts_with? app_assets_path2
-            true
-          else
-            if full_path.starts_with? app_assets_path3
-              true
-            else
-              false
-            end
-          end
-        end
-      end
-    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-config.serve_static_assets = true
-config.assets.compress = true
-config.assets.js _compressor = :uglifier
-config.assets.css_compressor = :sass
-config.assets.compile = false
-config.assets.digest = true
+# config.serve_static_assets = true
+# config.assets.compress = true
+# config.assets.js _compressor = :uglifier
+# config.assets.css_compressor = :sass
+# config.assets.compile = false
+# config.assets.digest = true
 # Initialize the Rails application.
 Rails.application.initialize!
