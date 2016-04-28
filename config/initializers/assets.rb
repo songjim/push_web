121f522f6640838512e837e4d8abe59caec9c105
songjian <songjian@baozou.com>
2016-04-28 21:52:37 +0800

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( 
-	liantu.png
+  liantu.png
   bootstrap.js metisMenu.js
   sb-admin-2.js app.css
   bootstrap.css metisMenu.css
